751a50d5d0f8318d29044ebf976064a36f7b7624
Spencer Krenz <spencerkrenz@Spencers-MacBook-Pro.local>
2025-11-20 02:29:44 -0800

    Update Supabase client env vars

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,5 +1,5 @@
 // lib/supabaseClient.ts
-import { createClient } from "@supabase/supabase-js";
+import { createClient } from "@supabase/supabase-js";""
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
